0d1dc3892c42c1ef8c1da044860d2e808f19d409
gtb3ar <147428034+gtb3ar@users.noreply.github.com>
2024-11-29 10:33:22 +0000

    Empty

--- website/models.py
+++
@@ -1,6 +0,0 @@
-from .import db
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    task = db.Column(db.String(300), unique = True)
-    complete = db.Column(db.Boolean, default = False)
